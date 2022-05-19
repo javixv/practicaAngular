804ebf68efa95322f17cc0f89cd5d63426f16fa1
javixv <javier.fz@outlook.com>
2022-05-18 22:08:52 -0600

    perfil de usuario

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -8,6 +8,7 @@ import { AccountSettingsComponent } from './account-settings/account-settings.co
 import { PromesasComponent } from './promesas/promesas.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
 import { AuthGuard } from '../guards/auth.guard';
+import { PerfilComponent } from './perfil/perfil.component';
 
 
 const routes: Routes = [
@@ -21,6 +22,7 @@ const routes: Routes = [
         {path: 'accountsettings', component : AccountSettingsComponent, data : {titulo : 'Setting'}},
         {path: 'promesas', component : PromesasComponent, data : {titulo : 'Promesas'}},
         {path: 'rxjs', component : RxjsComponent, data : {titulo : 'RxJs'}},
+        {path: 'perfil', component : PerfilComponent, data : {titulo : 'Perfil de Usuario'}},
       ]},
     //{ path: 'path/:routeParam', component: MyComponent },
     //{ path: 'staticPath', component: ... },
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -14,6 +14,7 @@ import { ComponentsModule } from '../components/components.module';
 import { AccountSettingsComponent } from './account-settings/account-settings.component';
 import { PromesasComponent } from './promesas/promesas.component';
 import { RxjsComponent } from './rxjs/rxjs.component';
+import { PerfilComponent } from './perfil/perfil.component';
 
 
 
@@ -25,7 +26,8 @@ import { RxjsComponent } from './rxjs/rxjs.component';
     PagesComponent,
     AccountSettingsComponent,
     PromesasComponent,
-    RxjsComponent
+    RxjsComponent,
+    PerfilComponent
   ],
   exports : [
     DashboardComponent,
