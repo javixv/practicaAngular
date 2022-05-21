a8d6cf71c8eb2631b0d84e235f808e4b8d79cf4a
javixv <javier.fz@outlook.com>
2022-05-20 22:43:30 -0600

    actializar usuario

--- src/app/pages/perfil/perfil.component.ts
+++ src/app/pages/perfil/perfil.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Usuarios } from 'src/app/models/usuarios.model';
 import { UsuarioService } from 'src/app/service/usuario.service';
 
 
@@ -12,12 +13,17 @@ import { UsuarioService } from 'src/app/service/usuario.service';
 export class PerfilComponent implements OnInit {
 
   public perfilForm! : FormGroup
-  constructor(private fb : FormBuilder,private usuarioService : UsuarioService) { }
+  public usuario! : Usuarios
+
+  constructor(private fb : FormBuilder,private usuarioService : UsuarioService) { 
+    this.usuario = this.usuarioService.usuario
+  }
+
 
   ngOnInit(): void {
     this.perfilForm = this.fb.group({
-      nombre : ['nombrte', Validators.required],
-      email : ['email', [Validators.required, Validators.email]]
+      nombre : [this.usuario.nombre, Validators.required],
+      email : [this.usuario.email, [Validators.required, Validators.email]]
     })
 
   }
@@ -25,6 +31,9 @@ export class PerfilComponent implements OnInit {
   guardarperfil(){
     console.log(this.perfilForm.value)
     this.usuarioService.actualizarPerfil(this.perfilForm.value).subscribe(resp => {
+      const {nombre, email} = this.perfilForm.value
+      this.usuario.nombre = nombre
+      this.usuario.email = email
       console.log(resp)
     })
   }
